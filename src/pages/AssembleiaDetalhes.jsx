f30421f094cf941f80f251ae5db92adbfb8118a1
Fabio Ribeiro <generic.ytch@gmail.com>
2025-12-05 01:42:45 +0000

    File changes

--- src/pages/AssembleiaDetalhes.jsx
+++ src/pages/AssembleiaDetalhes.jsx
@@ -47,7 +47,7 @@ import {
 } from "@/components/ui/select";
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import QRCode from 'react-qr-code';
+
 import Sidebar from '@/components/dashboard/Sidebar';
 import DashboardHeader from '@/components/dashboard/DashboardHeader';
 
