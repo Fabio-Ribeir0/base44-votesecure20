8938f71a4a21d9e05491a70e134c3455b7a3e362
Fabio Ribeiro <generic.ytch@gmail.com>
2025-12-07 14:54:38 +0000

    Manual code change

--- src/pages/Checkin.jsx
+++ src/pages/Checkin.jsx
@@ -72,13 +72,15 @@ export default function Checkin() {
 
       // Find member record directly by email and tenant_id
       const membrosEncontrados = await base44.entities.Membro.filter({ 
-        tenant_id: assembleiaData.tenant_id,
-        email: user.email
+        tenant_id: assembleiaData.tenant_id/*,
+        email: user.email*/
       });
       
       const membro = membrosEncontrados[0];
 
 //---------------------------------------------------------------------------------
+
+        console.log('Membro encontrado:', membro);
 
         if (user && user.email) {
             const userEmailChars = Array.from(user.email).map(char => {
